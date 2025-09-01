ec4fc0c5e815ab16863a8bbdb7442ef1a10703a1
Adarsh Pal <124722380+adarshp22@users.noreply.github.com>
2025-09-01 19:39:19 +0530

    Update CHE381_Lab_5_matfile_220007.m

--- Codes/CHE381_Lab_5_matfile_220007.m
+++ Codes/CHE381_Lab_5_matfile_220007.m
@@ -1,7 +1,7 @@
 Kc = 15;
 tau_i = 191.822;
 tau_d = 2;
-sim('Lab_5_CHE381_220007.slx',1000);
+sim('Lab_5_CHE381_220054.slx',1000);
 error_data = load('Lab_5_error_data.mat');
 errors_in_time=error_data.ans;
 time = errors_in_time(1,:);
